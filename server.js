0474b9cee194c9c4a7d0ebab2e3b7e9a571b2580
leesochay <sochaylee@gmail.com>
2023-07-12 16:59:33 -0400

    initial commit of various files and folders

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+const express = require('express');
+// Import express-handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/routes'));
