42ddffd72c9ca3cef951a63ffed453f958e2e6eb
leesochay <sochaylee@gmail.com>
2023-07-20 06:11:56 -0400

    updates user model and seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,28 @@
 const sequelize = require("../config/connection");
+const User = require("../models/User");
 const Blogpost = require("../models/Blogpost");
+const Comment = require("../models/Comment");
+
+const userData = require("./user-seeds.json");
 const blogpostData = require("./blogpost-seeds.json");
+const commentData = require("./comment-seeds.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Blogpost.bulkCreate(blogpostData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+  for (const blog of blogpostData) {
+    await Blogpost.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  const comments = await Comment.bulkCreate(commentData);
 
   process.exit(0);
 };
