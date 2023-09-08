5675fbf43559e0c958330cbfac35487bb77acc99
leesochay <sochaylee@gmail.com>
2023-09-08 14:37:21 -0400

    updating home controller and routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,58 +1,105 @@
 const router = require("express").Router();
-const Blogpost = require("../models/Blogpost");
-const Comment = require("../models/Comment");
-// route to get all blogposts
-router.get("/", async (req, res) => {
-  const blogpostData = await Blogpost.findAll().catch((err) => {
-    res.json(err);
-  });
-  // We use map() to iterate over blogpostData and then add .get({ plain: true }) each object to serialize it.
-  const blogposts = blogpostData.map((blogpost) =>
-    blogpost.get({ plain: true })
-  );
-  // We render the template, 'all', passing in blogposts, a new array of serialized objects.
-  res.render("homepage", { blogposts });
-});
+const { Blogpost, User, Comment } = require("../models");
+// const withAuth = require("../utils/auth");
 
-// get one blogpost with serialized data
-router.get("/blogpost/:id", async (req, res) => {
+// render the homepage
+router.get("/", async (req, res) => {
   try {
-    // Search the database for a blogpost with an id that matches params
-    const blogpostData = await Blogpost.findByPk(req.params.id);
-    if (!blogpostData) {
-      res.status(404).json({ message: "No blogpost with this id!" });
-      return;
-    }
-    // We use .get({ plain: true }) on the object to serialize it so that it only includes the data that we need.
+    // get all the blogposts and join with user data
+    const blogpostData = await Blogpost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ]
+    });
+    // serialize the data so the template can read it
+    const blogposts = blogpostData.map((blogpost) => blogpost.get({ plain: true }));
+
+    // pass the serialized data and session flag into the template
+    res.render("homepage", { 
+        blogposts, 
+        logged_in: req.session.logged_in
+        });
+      } catch (err) {
+      res.status(500).json(err);
+      }
+    });
+
+// Route to render individual post page
+
+router.get('/blogposts/:id', async (req, res) => {
+  try {
+  // Find post by ID with associated username
+    const blogpostData = await Blogpost.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ["comment_text", "user_id"],
+        },
+      ],
+    });
+
+// Convert post data to plain JavaScript object
     const blogpost = blogpostData.get({ plain: true });
-    // Then, the 'blogpost' template is rendered and blogpost is passed into the template.
-    res.render("blogpost", blogpost);
+	
+console.log(blogpost);
+    res.render('blogpost', {
+      ...blogpost,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [
+        {
+          model: Blogpost
+        },
+      ],
+    });
+
+    const blogposts = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...blogposts,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// login
 router.get("/login", (req, res) => {
-  // if (req.session.loggedIn) {
-  if (1 === 2) {
+  if (req.session.logged_in) {
     res.redirect("/");
     return;
   }
   res.render("login");
 });
 
+// signup
 router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
   res.render("signup");
 });
 
-router.get("/blogpost", async (req, res) => {
-  const commentData = await Comment.findAll().catch((err) => {
-    res.json(err);
-  });
-  // We use map() to iterate over blogpostData and then add .get({ plain: true }) each object to serialize it.
-  const comments = commentData.map((comment) => comment.get({ plain: true }));
-  // We render the template, 'all', passing in blogposts, a new array of serialized objects.
-  res.render("blogpost", { comments });
-});
 
 module.exports = router;
